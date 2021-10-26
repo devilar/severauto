003170b6f55fb360f819c5ef0b94b66ac39c0e6c
devilar <Cerberus2342aa>
2021-10-26 23:09:17 +0300

    new changes

--- src/components/HeaderProfile/HeaderProfile.tsx
+++ src/components/HeaderProfile/HeaderProfile.tsx
@@ -1,13 +1,59 @@
 import React from 'react';
-import Test from '../../Test';
-import ClickAway from './Test';
+import ClickAwayListener from '@mui/material/ClickAwayListener';
+import Box from '@mui/material/Box';
+import PersonIcon from '@mui/icons-material/Person';
+import { Btn } from '../Button/Button';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+import './headerProfile.scss';
 
 const HeaderProfile = () => {
+
+  const [open, setOpen] = React.useState(false);
+
+  const handleClick = () => {
+    setOpen((prev) => !prev);
+  };
+
+  const handleClickAway = () => {
+    setOpen(false);
+  };
+
+  const styles = {
+    position: 'absolute',
+    top: 28,
+    right: 0,
+    left: 0,
+    zIndex: 1,
+    border: '1px solid',
+    p: 1,
+    bgcolor: 'background.paper',
+  };
+
+  const person = "Ivan";
+
+
   return (
     <div className='HeaderProfile_container'>
 
 
-    <ClickAway/>
+      <ClickAwayListener onClickAway={handleClickAway}>
+        <Box sx={{ position: 'relative' }}>
+          <div className='person_inner' onClick={handleClick}>
+            <div>{person}</div><PersonIcon/>
+          </div>
+          {open ? (
+            <Box sx={styles}>
+              <nav>
+                <ul>
+                  <li><Btn fullWidth className='active'><Link to="/admin/ostatki">Открыть профиль</Link></Btn></li>
+                  <li><Btn fullWidth><Link to="/admin/postavshiki">Выход</Link></Btn></li>
+
+                </ul>
+              </nav>
+            </Box>
+          ) : null}
+        </Box>
+      </ClickAwayListener>
 
 
 
